a780adc627c90830c4d8c6236f9fd7190828acf2
25hours <shawnniu@yeah.net>
2017-05-31 23:04:34 +0800

    实现基于Django的admin自定义数据表

--- UserAuthentication/models.py
+++ UserAuthentication/models.py
@@ -1,61 +1,65 @@
 from django.db import models
-from django.core.exceptions import ValidationError
-from django.contrib.auth.models import User
-# Create your models here.
+from django.contrib.auth.models import (BaseUserManager,AbstractBaseUser)
 
-class UserProfile(models.Model):
-    user = models.OneToOneField(User)
-    email = models.EmailField(verbose_name='邮箱地址',max_length=255,unique=True)
-    phone = models.BigIntegerField(blank=True,null=True)
-    password = models.CharField(u'密码',max_length=128)
+class UserManager(BaseUserManager):
+    def create_user(self, email, name, password=None):
+        if not email:
+            raise ValueError('Users must have an email address')
+
+        user = self.model(
+            email = self.normalize_email(email),
+            name = name,
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, name, password):
+        user = self.create_user(email,password=password,name=name,)
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+
+class UserProfile(AbstractBaseUser):
+    email = models.EmailField(
+        verbose_name='邮件地址',
+        max_length=255,
+        unique=True,
+    )
+    # date_of_birth = models.DateField()
+    name = models.CharField(max_length=32)
+    is_active = models.BooleanField(default=True)
     is_admin = models.BooleanField(default=False)
-    # is_staff = models.BooleanField(
-    #     verbose_name='staff status',
-    #     default=True,
-    #     help_text='Designates whether the user can log into this admin site.',
-    # )
-    #role = models.ForeignKey("Role",verbose_name="权限角色")
-    memo = models.TextField('备注', blank=True, null=True, default=None)
 
-    # USERNAME_FIELD = 'email'
-    # REQUIRED_FIELDS = ['name','token','department','tel','mobile','memo']
-    # REQUIRED_FIELDS = ['name']
+    objects = UserManager()
 
-    # def get_full_name(self):
-    #     # The user is identified by their email address
-    #     return self.email
-    #
-    # def get_short_name(self):
-    #     # The user is identified by their email address
-    #     return self.email
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
 
-    def __str__(self):  # __str__ on Python 2
-        return u"%s,%s" %(self.email,self.is_admin)
+    def get_full_name(self):
+        # The user is identified by their email address
+        return self.email
 
-    # def has_perm(self, perm, obj=None):
-    #     "Does the user have a specific permission?"
-    #     # Simplest possible answer: Yes, always
-    #     return True
-    # def has_perms(self, perm, obj=None):
-    #     "Does the user have a specific permission?"
+    def get_short_name(self):
+        # The user is identified by their email address
+        return self.email
+
+    def __str__(self):              # __unicode__ on Python 2
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        "Does the user have a specific permission?"
         # Simplest possible answer: Yes, always
-        # return True
+        return True
 
-    # def has_module_perms(self, app_label):
-    #     "Does the user have permissions to view the app `app_label`?"
+    def has_module_perms(self, app_label):
+        "Does the user have permissions to view the app `app_label`?"
         # Simplest possible answer: Yes, always
-        # return True
+        return True
 
-
-    # @property
-    # def is_superuser(self):
-    #     "Is the user a member of staff?"
+    @property
+    def is_staff(self):
+        "Is the user a member of staff?"
         # Simplest possible answer: All admins are staff
-        # return self.is_admin
-
-
-    # objects = auth.UserManager()
-
-    class Meta:
-        verbose_name = ('用户信息')
-        verbose_name_plural = ('用户信息')
+        return self.is_admin
